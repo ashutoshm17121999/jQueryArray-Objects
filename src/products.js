bd31ec3173c8ed76942cb922fb3acc969946c3c0
Ashutoshm17121999 <ashutoshmishra2022jan@cedcoss.com>
2022-02-11 17:26:46 +0530

    Add to cart feature

--- src/products.js
+++ src/products.js
@@ -10,8 +10,42 @@ function display(products){
     <img src="images/'+products[i].image+'">\
     <h3 class="title"><a href="#">'+products[i].name+'</a></h3>\
     <span>Price: $'+products[i].price+'</span>\
-    <a class="add-to-cart" id="'+products[i].id+'href="#">Add To Cart</a></div>';
+    <a class="add-to-cart" data-icart="'+products[i].id+'" id="'+products[i].id+'href="#">Add To Cart</a></div>';
     // console.log("ashutosh");
 }
 $('#products').html(html);
 }
+$(document).ready(function(){
+    var prods;
+    temp =[];
+    $(document).on('click','.add-to-cart', function(){
+       prods= $(this).data('icart');
+       for(var i=0;i<products.length;i++){
+        
+           if(products[i].id==prods){
+               temp.push(products[i]);
+               console.log(products);
+           }
+       }
+       cartDisplay(temp)
+        // console.log("cedcoss");
+    })
+})
+
+function cartDisplay(pod){
+    var html1 ="";
+    html1 +='<table><tr><th>ID</th><th>Name</th><th>Price</th></tr>'
+    for(var i=0;i<pod.length;i++){
+        html1+='<tr>\
+        <td>'+pod[i].id+'</td>\
+        <td>'+pod[i].name+'</td>\
+        <td>'+pod[i].price+'</td>\
+        <td>\
+           <a href="#" class="Delete">Delete</a>\
+        </td>\
+        </tr>'
+    }
+    $("#myCart").html(html1);
+    // console.log("ashutosh");
+}
+
